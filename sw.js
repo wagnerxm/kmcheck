47e333b617badded7f882711a7d168a724ef9356
wagnerxm <wagnerxm.eng@hotmail.com>
2026-07-29 11:20:49 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kmcheck-v72';
+const CACHE = 'kmcheck-v79';
 const ASSETS = ['./', 'index.html', 'fflate.js', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png', 'logo-header.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
